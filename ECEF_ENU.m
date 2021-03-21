02b0d61bf2e1d533b7f86b12f3112a9ea8611feb
JeffDavis17 <47422520+JeffDavis17@users.noreply.github.com>
2021-03-20 21:02:08 -0400

    dgfhgjhj

--- ECEF_ENU.m
+++ ECEF_ENU.m
@@ -1,6 +1,6 @@
 %% Function converts ECEF to ENU
 
-function [e, n, u] = ECEF_ENU(x,y,z,lat,lon)
+function [enu] = ECEF_ENU(x,y,z,lat,lon)
 
 lat = deg2rad(lat);
 lon = deg2rad(lon);
@@ -10,11 +10,14 @@ r = [-sin(lat), cos(lat), 0;
     -cos(lat)*sin(lon), -sin(lat)*sin(lon), cos(lon);
     cos(lat)*cos(lon), sin(lat)*cos(lon), sin(lon)];
 
+enu = [];
+for i = 1:length(x(:))
+    val = r*[x(i);y(i);z(i)];
+    v1 = val(1);
+    v2 = val(2);
+    v3 = val(3);
 
-val = r*[x;y;z];
-e = val(1);
-n = val(2);
-u = val(3);
+    enu = [enu;v1,v2,v3];
 end
 
 
--- main.m
+++ main.m
@@ -3,15 +3,21 @@
 clear; clc; close all
 
 % Accurate location of stations (ECEF) HELLO
-ALBH = [-2341333.1131291,3539049.53605306,4745791.2613984];
-ALGO = [918129.141083288,4346071.33022714,4561977.91758169];
-BAKE = [289834.290606477,2756501.17332637,5725162.39460111];
+ALBH = [-2341333.1131291,-3539049.53605306,4745791.2613984];
+ALGO = [918129.141083288,-4346071.33022714,4561977.91758169];
+BAKE = [-289834.290606477,-2756501.17332637,5725162.39460111];
 %MAL = [];
 %SGO = [];
 %ZIMM = [];
 
+count = 1;
+locs = [ALBH;ALGO;BAKE]; 
+
 
 p = [];
+time_v = [];
+time_h = [];
+rms = [];
 files = fopen('read.txt');
 while (~feof(files))   
     file_name = textscan(files,'%s',1,'Delimiter','\n');
@@ -31,7 +37,6 @@ while (~feof(files))
     plot(time_vals,solutions.num_sat)
     ylabel('Number of Satellites'); ylim([0 25])
     % Plot DOP against time
-    %figure('position',[50 50 1000 600])
     yyaxis right
     plot(time_vals,solutions.GDOP)
     ylabel('GDOP'); ylim([0 5])
@@ -40,12 +45,54 @@ while (~feof(files))
     
     
     % Conversion to ENU for error
-    %
-    %
+    lat = solutions.llh(end,1);
+    lon = solutions.llh(end,2);
+    exact_enu = ECEF_ENU(locs(count,1),locs(count,2),locs(count,3),lat,lon); %Exact
+    enu = ECEF_ENU(solutions.ECEF(:,1),solutions.ECEF(:,2),solutions.ECEF(:,3),lat,lon);
+    
+
+    % Horizontal and Vertical Distances
+    horz = sqrt((enu(:,1) - exact_enu(1)).^2 + (enu(:,2)-exact_enu(2)).^2);
+    vert = sqrt((enu(:,3) - exact_enu(3)).^2);
+    hv = [horz,vert];
     
     
+    % Time Series of Horizontal and Vertical Errors
+    figure()
+    yyaxis left
+    plot(time_vals,horz)
+    ylabel('horizontal error (m)')
+    yyaxis right
+    plot(time_vals,vert)
+    ylabel('vertical errors (m)')
+    xlabel('time (hr)')
+    title('Vertical and Horizontal Error Evolution')
     
     
+    % Time to get less than 5 cents error
+    ind = hv <= 0.05;
+    v1 = find(ind(:,1)==1);
+    v2 = find(ind(:,2)==1);
+    
+    if ~isempty(v1)
+        t_h = 30*v1(1);
+        time_h = [time_h;t_h,count];
+    else
+        time_h = [time_h;999999999,count];
+    end
+    
+    if ~ isempty(v2)
+        t_v = 30*v2(1);
+        time_v = [time_v;t_v,count];
+    elseif size(v1) == 0
+        time_v = [time_v;999999999,count];
+    end
+    
+    % RMS Error
+    rms_val = sqrt(sum(hv.^2)/length(hv));
+    rms = [rms;rms_val];
+    
+    count = count+1;
 end
 
 
